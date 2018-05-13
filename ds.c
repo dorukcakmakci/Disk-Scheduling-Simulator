12a5407720c4b7a03354c53713daf4c99ed6073a
Doruk Cakmakci <dorukcakmakci97@gmail.com>
2018-05-13 19:46:59 +0300

    Final implementation

--- ds.c
+++ ds.c
@@ -7,7 +7,7 @@ double find_mean(int *arr, int size){
     for(int i = 0; i < size; i++){
         result += arr[i];
     }
-    result /= size;
+    result /= (size);
     return result;
 }
 
@@ -18,7 +18,7 @@ double find_stddev(int * arr, int size){
     for(int j = 0; j < size; j++){
         result += pow(arr[j] - mean,2);
     }
-    result /= size;
+    result /= (size - 1);
     result = sqrt(result);
     return result;
 }
@@ -84,9 +84,9 @@ void FCFS(int N, char* filename){
             continue;
         }
         if(currentCylinder == cylinder[currentIndex]){
-            requestGrantTime[currentIndex] = elapsedTime - oldElapsedTime + time[currentIndex];
+            requestGrantTime[currentIndex] += elapsedTime - time[currentIndex];//elapsedTime - oldElapsedTime + time[currentIndex];
             oldElapsedTime = elapsedTime;
-           printf("%d-------\n", numberOfRequestsFinished);
+            printf("%d-------\n", numberOfRequestsFinished);
             fflush(stdout);
             numberOfRequestsFinished++;
             currentIndex++;
@@ -94,6 +94,7 @@ void FCFS(int N, char* filename){
         }
         else if(currentCylinder < cylinder[currentIndex]){
             while(currentCylinder < cylinder[currentIndex]){
+                requestGrantTime[currentIndex]--;
                 currentCylinder++;
                 elapsedTime++;
             }
@@ -101,14 +102,19 @@ void FCFS(int N, char* filename){
         }
         else if(currentCylinder > cylinder[currentIndex]){
             while(currentCylinder > cylinder[currentIndex]){
+                requestGrantTime[currentIndex]--;
                 currentCylinder--;
                 elapsedTime++;
             }
             printf("4\n");
         }
     }
+    printf("linecount===%d\n", linecount);
 
     printf("FCFS\n----\nElapsed time: %d\nAverage wait time: %f\nStandard deviation of wait times: %f\n", elapsedTime, find_mean(requestGrantTime, linecount), find_stddev(requestGrantTime, linecount));
+    for(int i  = 0; i< linecount; i++){
+        printf("Request %d => Time: %d   Cylinder => %d Wait Time: %d\n", (i+1), time[i], cylinder[i], requestGrantTime[i]);
+    }
     fflush(stdout);
 
     fclose(fptr);
@@ -119,7 +125,7 @@ void FCFS(int N, char* filename){
      
 }
 
-int getMinimumNotTraversedIndex(int * request, int * granted, int headLocation, int size){
+int getMinimumNotTraversedIndex(int * request, int * granted, int headLocation, int size, int *time, int currentTime){
     int result = -1;
     int min = 999999;
     for(int i = 0; i < size; i++){
@@ -127,7 +133,7 @@ int getMinimumNotTraversedIndex(int * request, int * granted, int headLocation, 
             continue;
         }
         else{
-            if(abs(request[i] - headLocation) < min){
+            if(abs(request[i] - headLocation) < min && time[i] <= currentTime){
                 result = i;
                 min = abs(request[i] - headLocation);
             }
@@ -192,12 +198,19 @@ void SSTF(int N, char* filename){
     for(int i = 0; i < 100; i++){
         if(i >= linecount)
             grantRequest[i] = 1;
-        else 
+        else if(i == 0){
+            printf("yarrakBB\n");
+            grantRequest[i] = 1;
+            printf("yarrakAA\n");
+        }
+            
+        else
             grantRequest[i] = 0;
     }
     
-    int currentIndex = 0;
     int currentCylinder = 1;
+    int currentIndex = 0;
+    
 
     while(numberOfRequestsFinished < linecount){
 
@@ -207,13 +220,14 @@ void SSTF(int N, char* filename){
             continue;
         }
         if(currentCylinder == cylinder[currentIndex]){
-            requestGrantTime[currentIndex] = elapsedTime - oldElapsedTime + time[currentIndex];
+            
             oldElapsedTime = elapsedTime;
             printf("%d-------\n", numberOfRequestsFinished);
             fflush(stdout);
             numberOfRequestsFinished++;
-            currentIndex = getMinimumNotTraversedIndex(cylinder,  grantRequest, currentCylinder, linecount);
+            currentIndex = getMinimumNotTraversedIndex(cylinder,  grantRequest, currentCylinder, linecount, time, elapsedTime);
             grantRequest[currentIndex] = 1;
+            requestGrantTime[currentIndex] = elapsedTime - time[currentIndex];
             printf("2\n");
         }
         else if(currentCylinder < cylinder[currentIndex]){
@@ -233,16 +247,19 @@ void SSTF(int N, char* filename){
     }
 
     printf("SSTF\n----\nElapsed time: %d\nAverage wait time: %f\nStandard deviation of wait times: %f\n", elapsedTime, find_mean(requestGrantTime, linecount), find_stddev(requestGrantTime, linecount));
+    for(int i  = 0; i< linecount; i++){
+        printf("Request %d => Time: %d   Cylinder => %d Wait Time: %d\n", (i+1), time[i], cylinder[i], requestGrantTime[i]);
+    }
     fflush(stdout);
 
     fclose(fptr);
 
-    free(requestGrantTime); 
-    free(cylinder);
-    free(grantRequest); 
+    //free(requestGrantTime); 
+    //free(cylinder);
+    //free(grantRequest); 
 }
 
-int *getMaximumAndMinimumNotTraversedIndex(int * request, int * granted, int size){
+/*int *getMaximumAndMinimumNotTraversedIndex(int * request, int * granted, int size, int *time, int currentTime){
     int *result = malloc(2*sizeof(int));
     int max = -1;
     int min = 999999;
@@ -251,13 +268,47 @@ int *getMaximumAndMinimumNotTraversedIndex(int * request, int * granted, int siz
             continue;
         }
         else{
-            if(request[i] > max){
+            if(request[i] > max && time[i] < currentTime){
                 result[1] = i;
                 max = request[i];
             }
-            if(request[i] <  min){
+            if(request[i] <  min && time[i] < currentTime){
                 result[0] = i;
                 min = request[i];
+            }
+        }
+    }
+    return result;
+}*/
+// returns -1 if no such index
+int getNotGrantedIndexToTheRight(int * request, int * granted, int headLocation, int size, int *time, int currentTime){
+    int result = -1; 
+    int min = 999999;
+    for(int i = 0; i < size; i++){
+        if(granted[i] == 1){
+            continue;
+        }
+        else{
+            if(request[i] > headLocation && request[i] <  min && time[i] <= currentTime){
+                result = i;
+                min = request[i];
+            }
+        }
+    }
+    return result;
+}
+// returns -1 if no such index
+int getNotGrantedIndexToTheLeft(int * request, int * granted, int headLocation,  int size, int *time, int currentTime){
+    int result = -1; 
+    int max = -1;
+    for(int i = 0; i < size; i++){
+        if(granted[i] == 1){
+            continue;
+        }
+        else{
+            if(request[i] < headLocation && request[i] >  max && time[i] <= currentTime){
+                result = i;
+                max = request[i];
             }
         }
     }
@@ -311,6 +362,10 @@ void LOOK(int N, char* filename){
     int *requestGrantTime;
     requestGrantTime = malloc(100*sizeof(int));
 
+    for(int i = 0; i < 100; i++){
+        requestGrantTime[i] = 0;
+    }
+
     int *grantRequest;
     grantRequest =  malloc(100*sizeof(int));
 
@@ -320,61 +375,115 @@ void LOOK(int N, char* filename){
         else 
             grantRequest[i] = 0;
     }
-    int * max_min_indices = getMaximumAndMinimumNotTraversedIndex(cylinder,  grantRequest, linecount);
+
     int currentIndex = 0;
     int currentCylinder = 1;
 
+    int direction = 1; // 1--> right /// 0--> left
+    int tempR = -2;
+    int tempL = -2;
+
+    tempR = getNotGrantedIndexToTheRight(cylinder,  grantRequest, currentCylinder, linecount, time, elapsedTime);
+    tempL = getNotGrantedIndexToTheLeft(cylinder,  grantRequest, currentCylinder, linecount, time, elapsedTime);
+
     while(numberOfRequestsFinished < linecount){
+
+        
 
         if(elapsedTime < time[currentIndex]){
             elapsedTime++;
             printf("1\n");
             continue;
         }
-        if(currentCylinder < max_min_indices[1]){
-            while(currentCylinder < max_min_indices[1]){
+    
+        printf("TempR: %d  --  CurrentCylinder: %d  -- ", tempR, currentCylinder);
+        printf("TempL: %d --currentIndex: %d -- direction: %d \n", tempL, currentIndex, direction);
+        fflush(stdout);
+
+        if (direction == 1 && tempR != -1 ){
+            currentIndex = tempR;
+            while(currentCylinder < cylinder[currentIndex]){
+                requestGrantTime[currentIndex]--;
                 currentCylinder++;
                 elapsedTime++;
-                if(currentCylinder == cylinder[currentIndex]){
-                    requestGrantTime[currentIndex] = elapsedTime - oldElapsedTime + time[currentIndex];
+            }
+            if(currentCylinder == cylinder[currentIndex]){
+                    requestGrantTime[currentIndex] += elapsedTime - time[currentIndex];
+                    grantRequest[currentIndex] = 1;
                     oldElapsedTime = elapsedTime;
                     printf("%d-------\n", numberOfRequestsFinished);
                     fflush(stdout);
                     numberOfRequestsFinished++;
-                    currentIndex = getMinimumNotTraversedIndex(cylinder,  grantRequest, currentCylinder, linecount);
                     printf("2\n");
-                }
             }
             printf("3\n");
+            tempR = getNotGrantedIndexToTheRight(cylinder,  grantRequest, currentCylinder, linecount, time, elapsedTime);
+            tempL = getNotGrantedIndexToTheLeft(cylinder,  grantRequest, currentCylinder, linecount, time, elapsedTime);
+            if(tempR == -1)
+                direction--;
         }
-        else if(currentCylinder > max_min_indices[0]){
-            while(currentCylinder > max_min_indices[0]){
+         if (direction == 0 && tempL != -1 ){
+            
+            currentIndex = tempL;
+            while(currentCylinder > cylinder[currentIndex]){
+                requestGrantTime[currentIndex]--;
                 currentCylinder--;
                 elapsedTime++;
-                if(currentCylinder == cylinder[currentIndex]){
-                    requestGrantTime[currentIndex] = elapsedTime - oldElapsedTime + time[currentIndex];
+            }
+            if(currentCylinder == cylinder[currentIndex]){
+                    requestGrantTime[currentIndex] += elapsedTime - time[currentIndex];
                     oldElapsedTime = elapsedTime;
+                    grantRequest[currentIndex] = 1;
                     printf("%d-------\n", numberOfRequestsFinished);
                     fflush(stdout);
                     numberOfRequestsFinished++;
-                    currentIndex = getMinimumNotTraversedIndex(cylinder,  grantRequest, currentCylinder, linecount);
                     printf("2\n");
-                }
             }
             printf("4\n");
+            tempR = getNotGrantedIndexToTheRight(cylinder,  grantRequest, currentCylinder, linecount, time, elapsedTime);
+            tempL = getNotGrantedIndexToTheLeft(cylinder,  grantRequest, currentCylinder, linecount, time, elapsedTime);
+            if(tempL == -1){
+                direction++;
+            }
         }
+        if(tempR == -1 && tempL == -1){
+            break;
+        }
+
+        
     }
 
-    printf("SSTF\n----\nElapsed time: %d\nAverage wait time: %f\nStandard deviation of wait times: %f\n", elapsedTime, find_mean(requestGrantTime, linecount), find_stddev(requestGrantTime, linecount));
+    printf("LOOK\n----\nElapsed time: %d\nAverage wait time: %f\nStandard deviation of wait times: %f\n", elapsedTime, find_mean(requestGrantTime, linecount), find_stddev(requestGrantTime, linecount));
+    for(int i  = 0; i< linecount; i++){
+        printf("Request %d => Time: %d   Cylinder => %d Wait Time: %d\n", (i+1), time[i], cylinder[i], requestGrantTime[i]);
+    }
     fflush(stdout);
 
     fclose(fptr);
 
-    free(requestGrantTime); 
+    /*free(requestGrantTime); 
     free(cylinder);
-    free(grantRequest);
+    free(grantRequest);*/
 
      
+}
+
+// returns -1 if no such index
+int getNotGrantedMinimalIndexToTheLeft(int * request, int * granted, int headLocation,  int size, int *time, int currentTime){
+    int result = -1; 
+    int min = 999999;
+    for(int i = 0; i < size; i++){
+        if(granted[i] == 1){
+            continue;
+        }
+        else{
+            if(request[i] < headLocation && request[i] <  min && time[i] <= currentTime){
+                result = i;
+                min = request[i];
+            }
+        }
+    }
+    return result;
 }
 
 void CLOOK(int N, char* filename){
@@ -417,7 +526,103 @@ void CLOOK(int N, char* filename){
             printf("Request Ended => Time: %d   Cylinder => %d\n", time[j], cylinder[j]);
     }
 
-    //TODO
+    int elapsedTime = 0;
+    int oldElapsedTime = 0;
+    int numberOfRequestsFinished = 0;
+   
+    int *requestGrantTime;
+    requestGrantTime = malloc(100*sizeof(int));
+
+    for(int i = 0; i < 100; i++){
+        requestGrantTime[i] = 0;
+    }
+
+    int *grantRequest;
+    grantRequest =  malloc(100*sizeof(int));
+
+    for(int i = 0; i < 100; i++){
+        if(i >= linecount)
+            grantRequest[i] = 1;
+        else 
+            grantRequest[i] = 0;
+    }
+
+    int currentIndex = 0;
+    int currentCylinder = 1;
+
+    int direction = 1; // 1--> right /// 0--> left
+    int tempR = -2;
+    int tempL = -2;
+
+    tempR = getNotGrantedIndexToTheRight(cylinder,  grantRequest, currentCylinder, linecount, time, elapsedTime);
+
+    while(numberOfRequestsFinished < linecount){
+
+        
+
+        if(elapsedTime < time[currentIndex]){
+            elapsedTime++;
+            printf("1\n");
+            continue;
+        }
+    
+        printf("TempR: %d  --  CurrentCylinder: %d  -- ", tempR, currentCylinder);
+        fflush(stdout);
+
+        if (tempR != -1 ){
+            currentIndex = tempR;
+            while(currentCylinder < cylinder[currentIndex]){
+                requestGrantTime[currentIndex]--;
+                currentCylinder++;
+                elapsedTime++;
+            }
+            if(currentCylinder == cylinder[currentIndex]){
+                requestGrantTime[currentIndex] += elapsedTime - time[currentIndex];
+                grantRequest[currentIndex] = 1;
+                oldElapsedTime = elapsedTime;
+                printf("%d-------\n", numberOfRequestsFinished);
+                fflush(stdout);
+                numberOfRequestsFinished++;
+                printf("2\n");
+            }
+            printf("3\n");
+            tempR = getNotGrantedIndexToTheRight(cylinder,  grantRequest, currentCylinder, linecount, time, elapsedTime);
+        }
+        else if(tempR == -1){
+            
+            tempR = getNotGrantedMinimalIndexToTheLeft(cylinder,  grantRequest, currentCylinder, linecount, time, elapsedTime);
+            if(tempR  == -1){
+                    break;
+            }
+            while(currentCylinder > cylinder[tempR]){
+                currentCylinder--;
+                requestGrantTime[tempR]--;
+                /*for(int i = 0; i < linecount; i++){
+                    if(grantRequest[i] == 0 && time[i] <= elapsedTime){
+                        requestGrantTime[i]--;
+                    }
+                }*/
+                elapsedTime++;  
+            }
+            
+
+            printf("Left minimal index: %d -- current cykinder: %d -- num req: %d\n", tempR, currentCylinder, numberOfRequestsFinished);
+        }
+         
+        
+    }
+
+    printf("CLOOK\n----\nElapsed time: %d\nAverage wait time: %f\nStandard deviation of wait times: %f\n", elapsedTime, find_mean(requestGrantTime, linecount), find_stddev(requestGrantTime, linecount));
+    for(int i  = 0; i< linecount; i++){
+        printf("Request %d => Time: %d   Cylinder => %d Wait Time: %d\n", (i+1), time[i], cylinder[i], requestGrantTime[i]);
+    }
+    fflush(stdout);
+
+    fclose(fptr);
+
+    /*free(requestGrantTime); 
+    free(cylinder);
+    free(grantRequest);*/
 
     fclose(fptr);
 
@@ -435,17 +640,16 @@ int main(int argc, char ** argv){
     int N = argv[1];
     char * filename = argv[2];
    
-
     //Use FCFS to schedule
     //FCFS(N, filename);
     
     //Use SSTF to schedule
-    SSTF(N, filename);
+    //SSTF(N, filename);
 
     //Use LOOK to schedule
     //LOOK(N, filename);
 
     //Use CLOOK to schedule
-    //CLOOK(N, filename);
+    CLOOK(N, filename);
     return 0;
 }
